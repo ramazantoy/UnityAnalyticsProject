16a54d9cccff63b720308382f5a9e9fae9e69be6
Ramazan  TOY <ramcotoy06@gmail.com>
2024-09-05 17:29:26 +0300

    UnityAnalytics.Back : DbContext created, connection string added

---
+++ UnityAnalytics/UnityAnalytics.Back/Persistence/Context/UABackContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnityAnalytics.Back.Core.Domain;
+using UnityAnalytics.Back.Persistence.Configurations;
+
+namespace UnityAnalytics.Back.Persistence.Context;
+
+public class UABackContext : DbContext
+{
+    public UABackContext(DbContextOptions<UABackContext> contextOptions): base(contextOptions)
+    {
+        
+    }
+
+    public DbSet<AppUser> AppUsersUsers => Set<AppUser>();
+    public DbSet<AppRole> AppRoles => Set<AppRole>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AppUserConfiguration());
+    
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- UnityAnalytics/UnityAnalytics.Back/Program.cs
+++ UnityAnalytics/UnityAnalytics.Back/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using UnityAnalytics.Back.Core.Application.Interfaces;
+using UnityAnalytics.Back.Persistence.Context;
+using UnityAnalytics.Back.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<UABackContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Local"));
+});
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 var app = builder.Build();
 
